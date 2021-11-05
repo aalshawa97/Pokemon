4ac1fecfba5e144c4c19b108539101108b80d832
aalshawa97 <abdullahalshawa@gmail.com>
2021-11-05 16:54:26 -0700

    Added dagger

---
+++ app/src/main/java/com/example/pokemon/BaseApplication.java
@@ -0,0 +1,7 @@
+package com.example.pokemon;
+
+import android.app.Application;
+//import dagger.hilt.android.*;
+
+public class BaseApplication extends Application {
+}
--- app/src/main/java/com/example/pokemon/MainActivity.java
+++ app/src/main/java/com/example/pokemon/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pokemon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import dagger.hilt.android.AndroidEntryPoint;
 
 public class MainActivity extends AppCompatActivity {
 
